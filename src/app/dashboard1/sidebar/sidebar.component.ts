2b86be40b37272ebdace3aa951135ab1b65e9661
Likhila <likhilagadi@gmail.com>
2021-06-30 22:57:18 +0530

    first commit

---
+++ src/app/dashboard1/sidebar/sidebar.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+import * as $ from "jquery";
+import { ChangeDetectorRef,  OnDestroy } from '@angular/core';
+import { MediaMatcher } from '@angular/cdk/layout';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent implements OnInit {
+
+  
+  panelOpenState = false;
+  ngOnInit(): void {
+   
+  }
+  
+  mobileQuery: MediaQueryList;
+  
+  private _mobileQueryListener: () => void;
+
+  constructor(changeDetectorRef: ChangeDetectorRef, media: MediaMatcher) {
+    this.mobileQuery = media.matchMedia('(max-width: 600px)');
+    this._mobileQueryListener = () => changeDetectorRef.detectChanges();
+    this.mobileQuery.addListener(this._mobileQueryListener);
+  }
+
+  ngOnDestroy(): void {
+    this.mobileQuery.removeListener(this._mobileQueryListener);
+  }
+
+ 
+}
